c9f022762e1ec7607d1ae8e7475b18f6f1885bb7
NatashaOvdina <natashaovdina@yahoo.com>
2023-11-17 14:39:14 +0300

    Initial Commit

---
+++ TrafficLightTraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLightTraining
+//
+//  Created by Natalia Ovdina on 17.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
