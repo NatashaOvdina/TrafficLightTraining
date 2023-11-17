cfc9618da3c94e483dad71e261ccde0694148f1c
NatashaOvdina <natashaovdina@yahoo.com>
2023-11-17 15:13:32 +0300

    Made rounded corners in views and in a button.

--- TrafficLightTraining/ViewController.swift
+++ TrafficLightTraining/ViewController.swift
@@ -7,11 +7,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    
+    @IBOutlet var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        button.layer.cornerRadius = 10
+    }
+    
+    override func viewWillLayoutSubviews() {
+        redView.layer.cornerRadius = redView.frame.width / 2
+        yellowView.layer.cornerRadius = yellowView.frame.width / 2
+        greenView.layer.cornerRadius = greenView.frame.width / 2
     }
 
 
